b1f3d1e5c86d344f70d19fe61bc3af98144dd946
BuildTools <unconfigured@null.spigotmc.org>
2021-09-27 12:51:46 +0200

    Commit

--- Open-Lab-01.01/Exercise.cs
+++ Open-Lab-01.01/Exercise.cs
@@ -6,7 +6,17 @@ namespace Open_Lab_01._01
     {
         public bool IsLessThan10(string input)
         {
-            throw new NotImplementedException();
+            int number;
+            int.TryParse(input, out number);
+
+            if (number < 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
